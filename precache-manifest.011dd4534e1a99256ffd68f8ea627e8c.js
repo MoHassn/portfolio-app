35550f869f13d642ad8824cd84403e7a04ca5fdd
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2020-05-13 13:51:24 +0200

    Updates

---
+++ precache-manifest.011dd4534e1a99256ffd68f8ea627e8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfc264ca32066816e10d8cac42607e6",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "91f3a49e820c62c36bf0",
+    "url": "/portfolio-app/static/css/main.f66d6229.chunk.css"
+  },
+  {
+    "revision": "b5711e1d2073919d1615",
+    "url": "/portfolio-app/static/js/2.505b41ed.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/portfolio-app/static/js/2.505b41ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f3a49e820c62c36bf0",
+    "url": "/portfolio-app/static/js/main.2b814771.chunk.js"
+  },
+  {
+    "revision": "7297e3a932726fd6d9ee",
+    "url": "/portfolio-app/static/js/runtime-main.bca4c63d.js"
+  },
+  {
+    "revision": "e8112cd3b2f8d7cd940220dff2ca9fe0",
+    "url": "/portfolio-app/static/media/logo.e8112cd3.svg"
+  }
+]);
