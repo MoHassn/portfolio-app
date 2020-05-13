0574e6894cbc04e1f3e9177a52903ed56f61b001
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2020-05-13 06:32:18 +0200

    render ProjectDetails in a second page for each project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import ProjectDetails from "./components/ProjectDetails";
 import { Route } from "react-router-dom";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Projects />
           <Contact />
         </Route>
+        <Route path="/projects/:id" children={<ProjectDetails />} />
       </main>
       <footer>Made with ♥.</footer>
     </div>
